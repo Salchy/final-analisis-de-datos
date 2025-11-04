1313100fb183788e801af82ba392abca69db2bf5
Salchy <leandrocg524@gmail.com>
2025-11-04 11:58:52 -0300

    Add: Clase banco

---
+++ App/Banco.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Analisis_de_Datos.App
+{
+    public class Banco
+    {
+        public string Nombre { get; set; }
+        public float[] valoresHistoricos = new float[3];
+
+        // constructor
+        public Banco(string nombre)
+        {
+            Nombre = nombre;
+        }
+
+        // Métodos:
+
+        // Hacer que sólo hayan 3 valores historicos por banco
+        public void AgregarValorHistorico(int anio, float valor)
+        {
+            if (anio <= 0 || anio > 3)
+            {
+                throw new ArgumentOutOfRangeException("El año debe estar entre 1 y 3.");
+            }
+            valoresHistoricos[anio - 1] = valor;
+        }
+    }
+}

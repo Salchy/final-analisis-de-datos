357bb2242a19a2c2ca55a3528004b6d602b86be7
Salchy <leandrocg524@gmail.com>
2025-11-04 09:24:30 -0300

    Creacion del proyecto

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool closeApp = false;
+
+        while (!closeApp)
+        {
+            Console.Clear();
+            Console.WriteLine("===== MENÚ PRINCIPAL =====");
+            Console.WriteLine("1 - Opcion 1");
+            Console.WriteLine("2 - Opcion 2");
+            Console.WriteLine("0 - Salir");
+            Console.WriteLine("==========================");
+            Console.Write("Seleccione una opción: ");
+
+            string opcion = Console.ReadLine();
+            Console.WriteLine();
+
+            switch (opcion)
+            {
+                case "1":
+
+                    break;
+
+                case "2":
+
+                    break;
+
+                case "0":
+                    closeApp = true;
+                    Console.WriteLine("Saliendo del programa...");
+                    break;
+
+                default:
+                    Console.WriteLine("Opción no válida. Presione una tecla para continuar...");
+                    Console.ReadKey();
+                    break;
+            }
+        }
+    }
+}
